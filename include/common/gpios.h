7972aaa99a78be253cbd2cf079edd207ed26827e
juanmaher <jmhernandez1440@gmail.com>
2023-09-28 20:31:27 -0300

    Agrego fan button y seteo todos los buttons con pull down

--- include/common/gpios.h
+++ include/common/gpios.h
@@ -2,6 +2,7 @@
 #define GPIOS_H
 
 #define CRUSHER_BUTTON_GPIO     15
-#define MIXER_BUTTON_GPIO       16
+#define MIXER_BUTTON_GPIO       21
+#define FAN_BUTTON_GPIO         22
 
 #endif // GPIOS_H
--- src/hmi/buttons.c
+++ src/hmi/buttons.c
@@ -16,7 +16,7 @@
 
 static const char *TAG = "AC_Buttons";
 
-static button_t crusher_btn, mixer_btn;
+static button_t crusher_btn, mixer_btn, fan_btn;
 
 static const char *states[] = {
     [BUTTON_PRESSED]      = "pressed",
@@ -27,28 +27,38 @@ static const char *states[] = {
 
 static void on_crusher_button(button_t *btn, button_state_t state);
 static void on_mixer_button(button_t *btn, button_state_t state);
+static void on_fan_button(button_t *btn, button_state_t state);
 
 void Buttons_Start() {
     if (DEBUG) ESP_LOGI(TAG, "on %s", __func__);
 
-    // First button connected between GPIO and GND
-    // pressed logic level 0, no autorepeat
+    // Button connected between GPIO and +3.3V
+    // pressed logic level 1, no autorepeat
     crusher_btn.gpio = CRUSHER_BUTTON_GPIO;
-    crusher_btn.pressed_level = 0;
+    crusher_btn.pressed_level = 1;
     crusher_btn.internal_pull = true;
     crusher_btn.autorepeat = false;
     crusher_btn.callback = on_crusher_button;
 
-    // Second button connected between GPIO and +3.3V
-    // pressed logic level 1, autorepeat enabled
+    // Button connected between GPIO and +3.3V
+    // pressed logic level 1, no autorepeat
     mixer_btn.gpio = MIXER_BUTTON_GPIO;
     mixer_btn.pressed_level = 1;
     mixer_btn.internal_pull = true;
-    mixer_btn.autorepeat = true;
+    mixer_btn.autorepeat = false;
     mixer_btn.callback = on_mixer_button;
+
+    // Button connected between GPIO and +3.3V
+    // pressed logic level 1, no autorepeat
+    fan_btn.gpio = FAN_BUTTON_GPIO;
+    fan_btn.pressed_level = 1;
+    fan_btn.internal_pull = true;
+    fan_btn.autorepeat = false;
+    fan_btn.callback = on_fan_button;
 
     ESP_ERROR_CHECK(button_init(&crusher_btn));
     ESP_ERROR_CHECK(button_init(&mixer_btn));
+    ESP_ERROR_CHECK(button_init(&fan_btn));
 }
 
 static void on_crusher_button(button_t *btn, button_state_t state) {
@@ -64,5 +74,13 @@ static void on_mixer_button(button_t *btn, button_state_t state) {
 
     if (state == BUTTON_PRESSED) {
         esp_event_post(MIXER_EVENT, MIXER_EVENT_MANUAL_ON, NULL, 0, portMAX_DELAY);
+    }
+}
+
+static void on_fan_button(button_t *btn, button_state_t state) {
+    ESP_LOGI(TAG, "Fan button %s", states[state]);
+
+    if (state == BUTTON_PRESSED) {
+        esp_event_post(FAN_EVENT, FAN_EVENT_MANUAL_ON, NULL, 0, portMAX_DELAY);
     }
 }
