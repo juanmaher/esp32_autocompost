326fbd09b522579b529b5288c25f3d3b4e5cdb9d
juanmaher <jmhernandez1440@gmail.com>
2023-11-18 19:22:29 -0300

    fix: Asegurar que pull down y pull up esten deshabilitado
    El GPIO elijo no permite pull up o pull down internos. Hay que agregarlo externamente.

--- src/sensors/lid_sensor.c
+++ src/sensors/lid_sensor.c
@@ -64,14 +64,16 @@ static void lid_sensor_task(void* arg) {
 void LidSensor_Start() {
     //zero-initialize the config structure.
     gpio_config_t io_conf = {};
-    //interrupt of rising edge
+    //interrupt of any edge
     io_conf.intr_type = GPIO_INTR_ANYEDGE;
     //bit mask of the pins, use GPIO34 here
     io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
     //set as input mode
     io_conf.mode = GPIO_MODE_INPUT;
-    //enable pull_up mode
-    io_conf.pull_up_en = 1;
+    //disable pull_up mode
+    io_conf.pull_up_en = 0;
+    //disable pull_down mode
+    io_conf.pull_down_en = 0;
     gpio_config(&io_conf);
 
     current_gpio_state = gpio_get_level(GPIO_INPUT_IO_0);
